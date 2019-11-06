681beb539a68de8c4cd9d386abe72097317237dc
Chuck5ta <chuckstarrrr@googlemail.com>
2019-11-06 15:08:01 +0000

    Restructured the dircetory system.

---
+++ MyEngine/src/CoreLibrary/Main.cpp
@@ -0,0 +1,10 @@
+#pragma once
+/*
+    This is the entry point. It's where the execution of this engine first kicks off
+*/
+
+
+int main(int argc, char** argv)
+{
+    // start the application/engine
+}
